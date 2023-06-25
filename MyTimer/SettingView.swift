62e354a6ac861c25d64559c69c9486906b5146f7
tommy <tmtmtoa@gmail.com>
2023-06-25 22:43:32 +0900

    ピッカー配置

--- MyTimer/SettingView.swift
+++ MyTimer/SettingView.swift
@@ -3,8 +3,41 @@
 import SwiftUI
 
 struct SettingView: View {
+    // 秒数設定
+    @State var timerValue = 10
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        ZStack {
+            Color("backgroundSetting")
+                .ignoresSafeArea()
+            
+            VStack {
+                Spacer()
+                Text("\(timerValue)秒")
+                    .font(.largeTitle)
+                Spacer()
+                // Pickerを表示
+                Picker(selection: $timerValue) {
+                    Text("10")
+                        .tag(10)
+                    Text("20")
+                        .tag(20)
+                    Text("30")
+                        .tag(30)
+                    Text("40")
+                        .tag(40)
+                    Text("50")
+                        .tag(50)
+                    Text("60")
+                        .tag(60)
+                } label: {
+                    Text("選択")
+                }
+                // Pickerをホイール表示
+                .pickerStyle(.wheel)
+                Spacer()
+            }
+        }
     }
 }
 
